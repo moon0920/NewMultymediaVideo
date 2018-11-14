ce384780ba0bcf70cc2d702d809dddd476aeddc4
moon0920 <moon0920@dreamwiz.com>
2018-11-14 14:23:54 +0900

    Initial commit

---
+++ app/src/main/java/com/example/edu/newmultymediavideo/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.edu.newmultymediavideo;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnTaken = findViewById(R.id.btnTaken);
+        btnTaken.setOnClickListener(this);
+
+        }
+
+    @Override
+    public void onClick(View v) {
+        if(getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
+            Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+            startActivityForResult(intent, 100);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RESULT_OK) {
+            Uri videoUri = data.getData();
+            Toast.makeText(this, ""+videoUri,Toast.LENGTH_SHORT);
+            videoView = (VideoView)findViewById(R.id.videoView);
+            videoView.setVideoURI(videoUri);
+            videoView.start();
+        }
+    }
+}
